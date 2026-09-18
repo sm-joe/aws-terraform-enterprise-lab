f84020df392466276d0a1e91929fa886fb0451fb
sm_joe <sm_joe@rediffmail.com>
2026-09-18 22:12:33 +0530

    test: update workflows

--- environments/dev/s3/main.tf
+++ environments/dev/s3/main.tf
@@ -1,7 +1,7 @@
 module "dev_s3" {
   source = "../../../modules/s3"
 
-  bucket_name = "aws-terraform-enterprise-dev-s3-123234567678"
+  bucket_name = "aws-terraform-enterprise-lab-dev-s3-${var.aws_region}-123234567678"
 
   versioning_enabled = false
   force_destroy      = true
@@ -10,7 +10,7 @@ module "dev_s3" {
     Project     = "aws-terraform-enterprise"
     Environment = "dev"
     Component   = "s3"
-    Region      = "us-east-1"
+    Region      = var.aws_region
     ManagedBy   = "Terraform"
   }
 }
--- environments/dev/s3/variables.tf
+++ environments/dev/s3/variables.tf
@@ -4,7 +4,20 @@ variable "environment" {
   default     = "dev"
 }
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region where resources are deployed."
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
+
+  validation {
+    condition = contains(
+      [
+        "us-east-1",
+        "ap-south-1",
+        "eu-central-1"
+      ],
+      var.aws_region
+    )
+
+    error_message = "aws_region must be us-east-1, ap-south-1, or eu-central-1."
+  }
 }
